680e738b3cac07d5ff7af8245dea6fae2e303917
Flavio <fjcf@poli.br>
2022-09-19 12:44:06 -0300

    css route cart

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -5,12 +5,22 @@ import {
   ComplementAdress,
   Container,
   ContainerAdress,
+  ContainerButton,
   ContainerCoffeSelected,
+  ContainerDiv,
+  ContainerPrice,
+  ContainerRemove,
+  ContainerTypeCoffe,
+  ContainerTypeCoffeInformation,
   ContentAdress,
   ContentInformation,
   ContentPayment,
+  ItemsInformation,
+  OrderConfirm,
   PaymentText,
+  SpanButton,
   Street,
+  TotalItems,
   TypesPayment,
 } from "./styles";
 
@@ -75,7 +85,62 @@ export function Cart() {
           </TypesPayment>
         </ContentPayment>
       </ContainerAdress>
-      <ContainerCoffeSelected></ContainerCoffeSelected>
+      
+      <ContainerDiv>
+        <ContainerCoffeSelected>
+          <ContainerTypeCoffe>
+            <img src="/expressoTradicional.png" alt="" />
+            <ContainerTypeCoffeInformation>
+              <p>Expresso Tradicional</p>
+              <ContainerButton>
+                <SpanButton>
+                  <button>-</button>0<button>+</button>
+                </SpanButton>
+                <ContainerRemove>
+                  <img src="/trash.png" />
+                  <p>Remover</p>
+                </ContainerRemove>
+              </ContainerButton>
+            </ContainerTypeCoffeInformation>
+            <ContainerPrice>
+              <p>R$ 9,90</p>
+            </ContainerPrice>
+          </ContainerTypeCoffe>
+          <ContainerTypeCoffe>
+            <img src="/latte.png" alt="" />
+            <ContainerTypeCoffeInformation>
+              <p>Latte</p>
+              <ContainerButton>
+                <SpanButton>
+                  <button>-</button>0<button>+</button>
+                </SpanButton>
+                <ContainerRemove>
+                  <img src="/trash.png" />
+                  <p>Remover</p>
+                </ContainerRemove>
+              </ContainerButton>
+            </ContainerTypeCoffeInformation>
+            <ContainerPrice>
+              <p>R$ 9,90</p>
+            </ContainerPrice>
+          </ContainerTypeCoffe>
+          <TotalItems>
+            <ItemsInformation>
+              <p>Total de itens</p>
+              <p>R$ 29,70</p>
+            </ItemsInformation>
+            <ItemsInformation>
+              <p>Entrega</p>
+              <p>R$ 3,50</p>
+            </ItemsInformation>
+            <ItemsInformation>
+              <p><strong>Total</strong></p>
+              <p><strong>R$ 33,20</strong></p>
+            </ItemsInformation>
+          </TotalItems>
+          <OrderConfirm>confirmar pedido</OrderConfirm>
+        </ContainerCoffeSelected>
+      </ContainerDiv>
     </Container>
   );
 }
--- src/pages/cart/styles.ts
+++ src/pages/cart/styles.ts
@@ -9,14 +9,15 @@ export const Container = styled.div`
 export const ContainerAdress = styled.div`
   display: flex;
   flex-direction: column;
+
   width: 640px;
   height: 100%;
   box-sizing: border-box;
 
   &::before {
     content: "Complete seu pedido";
-
     margin-bottom: 15px;
+    height: 20px;
     font-family: "Baloo 2";
     font-style: normal;
     font-weight: 700;
@@ -244,12 +245,173 @@ export const TypesPayment = styled.div`
   }
 `;
 
+export const ContainerDiv = styled.div`
+  width: 640px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  &::before {
+    content: "Complete seu pedido";
+    height: 20px;
+    font-family: "Baloo 2";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 130%;
+    color: ${(props) => props.theme.colors["base-title"]};
+  }
+`;
+
 export const ContainerCoffeSelected = styled.div`
   display: flex;
+  flex-direction: column;
+  gap: 24px;
   box-sizing: border-box;
   width: 448px;
-  height: 505px;
+  min-height: 505px;
+  margin-top: 14px;
   padding: 40px;
   background: ${(props) => props.theme.colors["base-card"]};
   border-radius: 6px 44px;
+
+  & div img {
+    width: 64px;
+    height: 64px;
+  }
+`;
+
+export const ContainerTypeCoffe = styled.div`
+  display: flex;
+  width: 368px;
+  height: 90px;
+  border-bottom: 1px solid #e6e5e5;
+  padding-bottom: 24px;
+  justify-content: space-between;
+`;
+
+export const ContainerTypeCoffeInformation = styled.div`
+  & p:first-child {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 130%;
+    display: flex;
+    align-items: center;
+    color: #403937;
+  }
+`;
+
+export const SpanButton = styled.div`
+  display: flex;
+  align-items: center;
+  width: 72px;
+  height: 32px;
+  background: ${(props) => props.theme.colors["base-button"]};
+  gap: 3px;
+
+  button {
+    border: none;
+    background: ${(props) => props.theme.colors["base-button"]};
+    color: ${(props) => props.theme.colors["brand-purple"]};
+    width: 24px;
+    height: 24px;
+    display: flex;
+    align-items: center;
+    font-size: 24px;
+    cursor: pointer;
+  }
+`;
+
+export const ContainerButton = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+export const ContainerRemove = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 91px;
+  height: 32px;
+  background: ${(props) => props.theme.colors["base-button"]};
+  gap: 4px;
+  cursor: pointer;
+
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 160%;
+  text-transform: uppercase;
+  color: #574f4d;
+
+  & img {
+    width: 16px !important;
+    height: 16px !important;
+  }
+`;
+
+export const ContainerPrice = styled.div`
+  display: flex;
+  text-align: start;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 130%;
+`;
+
+export const TotalItems = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ItemsInformation = styled.div`
+  display: flex;
+  width: 368px;
+  justify-content: space-between;
+
+  & p {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 130%;
+    color: #574f4d;
+  }
+
+  & p strong {
+    font-family: "Roboto";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 130%;
+    color: #403937;
+  }
+`;
+
+export const OrderConfirm = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 12px 8px;
+  gap: 4px;
+  width: 368px;
+  height: 46px;
+  border: none;
+  cursor: pointer;
+  background: #dbac2c;
+  border-radius: 6px;
+
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 160%;
+  text-transform: uppercase;
+  color: #ffffff;
+  font-stretch: 100;
 `;
