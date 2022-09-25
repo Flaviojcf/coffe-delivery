ade5110dbf1d79efa33afa15a1172dfcfe551adb
Flavio <fjcf@poli.br>
2022-09-25 14:00:17 -0300

    deploy

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { DeafaultLayout } from "./defaultLayout/DefaultLayout";
-import { Cart } from "./pages/Cart/Cart";
+import { Cart } from "./pages/CartPage/Cart";
+
 import { Home } from "./pages/Home/Home";
 import { Success } from "./pages/Success/Sucess";
 
